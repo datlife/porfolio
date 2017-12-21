6bd01db31b3a57e17f7f293dd64785be1baf23ef
Dat Nguyen <tdat.nguyen93@gmail.com>
2017-12-21 13:36:54 -0800

    Backing up..

--- src/App.js
+++ src/App.js
@@ -4,19 +4,16 @@ import Footer from './components/Footer';
 import Main from './Routes';
 
 // for History
-import { BrowserRouter } from 'react-router-dom';
-
+import {BrowserRouter} from 'react-router-dom';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <div  style={{ marginTop: '3em' }} >
-          <div className="App">
-            <MenuBar />
-            <Main />
-            <Footer />
-          </div>
+        <div className="App">
+          <MenuBar/>
+          <Main/>
+          <Footer/>
         </div>
       </BrowserRouter>
     );
--- src/layouts/HomePage.js
+++ src/layouts/HomePage.js
@@ -1,44 +1,43 @@
 import React from 'react';
 
 let bio = {
-  "img":"https://avatars2.githubusercontent.com/u/13214917?s=460&v=4",
-  "about": "\"Hello, thank you for visting my website. \n\
-      I am currently a Computer Science junior at UC Davis. \
-      My work is currently on solving Computer Vison / Machine Learning problems \
-      through latest research in the field.\"",
-   "github": "https://github.com/datlife",
-   "linkedin": "https://www.linkedin.com/in/tdatnguyen/",
-   "twitter": "https://twitter.com/dat_ai_s"
-}
+  'img':      'https://avatars2.githubusercontent.com/u/13214917?s=460&v=4',
+  'github':   'https://github.com/datlife',
+  'linkedin': 'https://www.linkedin.com/in/tdatnguyen/',
+  'twitter':  'https://twitter.com/dat_ai_',
+  'email':    'tdat.nguyen93@gmail.com',
+  'about':    '"Hello, thank you for visting my website.' +
+              'I am currently a Computer Science junior at UC Davis.' +
+              'My work is currently on solving Computer Vison / Machine Learning problems' +
+              'through latest research in the field. "',
+} 
 
 class HomePage extends React.Component {
   render() {
     return (
       <div className='Home'>
         <div className='Bio'>
-          <div className="Photo">
-            <p> Dat T. Nguyen </p>
-            <img src={bio["img"]} />
-
-            <div className="Brands">
+          <div className='Photo'>
+            <img src={bio['img']} />
+            <div className='Brands'>
               <ul>
-                <a href={bio['github']}><li className="fab fa-github     fa-2x"/></a>
-                <a href={bio['linkedin']}><li className="fab fa-linkedin fa-2x"/></a>
-                <a href={bio['twitter']}><li className="fab fa-twitter   fa-2x"/></a>
+                 <li> <a href={bio['github']}><object data='assets/icons/github.svg' type='image/svg+xml' /> </a></li>
+                <a href={bio['linkedin']}><li className='fab fa-linkedin fa-2x'/></a>
+                <a href={bio['twitter']}> <li className='fab fa-twitter  fa-2x'/></a>
               </ul>
             </div>
           </div>
 
-          <div className="Pitch">
+          <div className='Pitch'>
             <p>{bio['about']}</p>
           </div>
-
         </div>
-        <div className="News">
+        <div className='News'>
           What I am up to
         </div>
-        <div className="Live">
-          My lastest info 
+
+        <div className='Live'>
+          Feed news (twitter, facebook, etc.) 
         </div>
 
       </div>
